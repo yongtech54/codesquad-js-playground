e139805ceffef41684790141b07ded3234bd25c2
yongtech54 <yongtech54@gmail.com>
2022-10-25 23:37:05 +0900

    [GUI 계산기]  화면 출력 기능 구현
    - 버튼을 눌러 입력 받는대로 실시간으로 화면에 출력

--- calculator/gui-cal.js
+++ calculator/gui-cal.js
@@ -1,35 +1,42 @@
 // input 객체 관련 속성 및 메소드 - 완료
 var input = {};
 input.array = [];
+
 input.clickNumbers = function (event) {
   var inputNumber = event.target.innerHTML;
   this.array.push(inputNumber);
+  output.display();
 };
+
 input.clickOperator = function (event) {
   var inputOperator = " " + event.target.innerHTML + " ";
   this.array.push(inputOperator);
+  output.display();
 };
+
 input.prepareCalculate = function () {
   this.array = this.array.join("").split(" ");
 };
+
 input.isEmpty = function () {
   return this.array.length;
 };
+
 input.removeValue = function () {
   this.array.pop();
+  output.display();
 };
 
+// calculator 객체 관련 속성 및 메소드 - 완료
 var calculator = {};
 calculator.getNumber = function () {
   var value = Number(input.array.shift());
   return value;
 };
-
 calculator.getOperator = function () {
   var value = input.array.shift();
   return value;
 };
-
 calculator.calculate = function (res, op, nextNumber) {
   switch (op) {
     case "+":
@@ -49,6 +56,21 @@ calculator.calculate = function (res, op, nextNumber) {
 };
 
 
+var output = {};
+output.display = function () {
+  var result = document.getElementById("result");
+  result.innerHTML = input.array.join("");
+
+  if (input.array.length === 0) {
+    result.innerHTML = "EMPTY";
+  }
+}
+
+output.print = function (res) {
+  var result = document.getElementById("result");
+  result.innerHTML = res;
+}
+
 // main logic
 var runCalculator = function () {
   input.prepareCalculate();
@@ -61,5 +83,5 @@ var runCalculator = function () {
     res = calculator.calculate(res, op, nextNumber);
   }
 
-  console.log(">>> " + res);
+  output.print(res);
 };
